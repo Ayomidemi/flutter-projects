cd96aebb77273dd637bc6c853b4babe81ee40407
Pease Adeniji <adenijiayomide13@gmail.com>
2023-09-11 00:26:23 +0100

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,32 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Brew Crew'),
+        centerTitle: true,
+        backgroundColor: Colors.brown[400],
+      ),
+      body: Center(
+        child: Text(
+          'Hello, World!',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: 'IndieFlower',
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('Click'),
+        backgroundColor: Colors.brown[400],
+      ),
+    ),
+  ));
 }
 
 
