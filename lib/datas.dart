09cde94334dd79ddef4f771588f5acdcb877293e
Pease Adeniji <adenijiayomide13@gmail.com>
2023-09-10 23:09:19 +0100

    cleanup for bre project

--- lib/datas.dart
+++
@@ -1,7 +0,0 @@
-class Datas {
-
-  String text;
-  String author;
-
-  Datas({ required this.text,  required this.author});
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:my_data_app/pages/choose_location.dart';
-import 'package:my_data_app/pages/home.dart';
-import 'package:my_data_app/pages/loading.dart';
 
 void main() {
-  runApp(MaterialApp(
-    initialRoute: '/',
-    routes: {
-      '/': (context) => Loading(),
-      '/home': (context) => Home(),
-      '/location': (context) => ChooseLocation(),
-    },
-  ));
+  runApp(MaterialApp());
 }
 
 
